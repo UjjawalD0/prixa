d3009da064f89ad79234e8a4787f4b6202f59d13
UjjawalD0 <122686931+UjjawalD0@users.noreply.github.com>
2023-02-21 20:45:11 +0545

    1

---
+++ website/js/main.js
@@ -0,0 +1,15 @@
+// $(window).scroll(function() {
+//     if ($(this).scrollTop()>10)
+//      {
+//         $('.top_part').hide(400);
+//         document.getElementById("navbar_part").style.marginTop = "0px";
+//      }
+//     else
+//      {
+//       $('.top_part').show(400);
+//         document.getElementById("navbar_part").style.marginTop = "40px";
+
+//      }
+//  });
+
+
