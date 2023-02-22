ebbcc69c9f8bf659b868d3582194be590429eee1
UjjawalD0 <122686931+UjjawalD0@users.noreply.github.com>
2023-02-22 20:36:55 +0545

    2

--- website/js/main.js
+++ website/js/main.js
@@ -13,3 +13,30 @@
 //  });
 
 
+$('.owl-carousel').owlCarousel({
+          animateOut: 'slideOutDown',
+          animateIn: 'flipInX',
+          items:1,
+          margin:50,
+          stagePadding:30,
+          smartSpeed:450,
+          loop:true,
+          // margin:10,
+          // nav:true,
+          navText: ['<i class="fa-solid fa-angle-left"></i>','<i class="fa-solid fa-angle-right"></i>'],
+          autoplay:true,
+          autoplayTimeout:2000,
+          autoplayHoverPause:true,
+          responsive:{
+              0:{
+                  items:1,
+              },
+              // 600:{
+              //     items:1,
+              //     // nav:true,
+              // },
+              // 1000:{
+              //     items:5
+              // }
+          }
+      });
